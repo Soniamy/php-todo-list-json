cdf8c0e7abb42e5c35177fb734c0636249abc42d
Soniamy <marianna.marzullo@gmail.com>
2024-02-13 20:47:26 +0100

    Fine esercizio di venerdì 9 febbraio con bonus.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ createApp({
     return {
       apiUrl: 'http://localhost:8888/feb-24/php-todo-list-json/server.php',
       listTasks: [],
+      newTask: '',
+      errorMsg: '',
     };
   },
 
@@ -13,6 +15,32 @@ createApp({
       axios.get(this.apiUrl).then((result) => {
         this.listTasks = result.data;
         console.log(result.dat);
+      });
+    },
+    addTask(event) {
+      // Creo oggetto formdata
+      event.preventDefault();
+      const data = new FormData();
+      // aggiungo variabile all'oggetto
+      data.append('text', this.newTask);
+      data.append('check', false);
+      // chiamata axios
+      axios.post(this.apiUrl, data).then((result) => {
+        this.newTask = '';
+        this.listTasks = result.data;
+        console.log(this.listTasks);
+      });
+    },
+    // Funzione Rimuovi task
+
+    remtsk(index) {
+      // Creo oggetto formdata
+      const data = new FormData();
+      // aggiungo variabile all'oggetto
+      data.append('delIndex', index);
+      // chiamata axios
+      axios.post(this.apiUrl, data).then((result) => {
+        this.listTasks = result.data;
       });
     },
   },
